9820c8ce12c6cc2ab6aca20c3274c11566a50116
supernovaw <ruszrj@gmail.com>
2022-12-27 12:00:24 -0800

    add notes field to images and tags validation

--- src/sample-data.js
+++ src/sample-data.js
@@ -1,10 +1,10 @@
 export const gallerySample = {
   images: [
-    { name: "PXL_20220618_042053450.jpg", filename: "PXL_20220618_042053450_mid.jpg", timestamp: 1655526053450, tags: ["outside", "sunset-sunrise", "beautiful-sky"] },
-    { name: "PXL_20220627_033606927.jpg", filename: "PXL_20220627_033606927_low.jpg", timestamp: 1656300966927, tags: ["cat", "my-room"] },
-    { name: "PXL_20220723_043722891.jpg", filename: "PXL_20220723_043722891_high.jpg", timestamp: 1658551042891, tags: ["outside", "sunset-sunrise", "landscape", "beautiful-sky"] },
-    { name: "PXL_20220816_014526261.jpg", filename: "PXL_20220816_014526261_low.jpg", timestamp: 1660614326261, tags: ["my-room", "body-in-view", "food"] },
-    { name: "PXL_20221203_181515838.jpg", filename: "PXL_20221203_181515838_mid.jpg", timestamp: 1670091315838, tags: ["outside", "landscape"] },
+    { name: "PXL_20220618_042053450.jpg", filename: "PXL_20220618_042053450_mid.jpg", timestamp: 1655526053450, tags: ["outside", "sunset-sunrise", "beautiful-sky"], notes: "" },
+    { name: "PXL_20220627_033606927.jpg", filename: "PXL_20220627_033606927_low.jpg", timestamp: 1656300966927, tags: ["cat", "my-room"], notes: "" },
+    { name: "PXL_20220723_043722891.jpg", filename: "PXL_20220723_043722891_high.jpg", timestamp: 1658551042891, tags: ["outside", "sunset-sunrise", "landscape", "beautiful-sky"], notes: "" },
+    { name: "PXL_20220816_014526261.jpg", filename: "PXL_20220816_014526261_low.jpg", timestamp: 1660614326261, tags: ["my-room", "body-in-view", "food"], notes: "" },
+    { name: "PXL_20221203_181515838.jpg", filename: "PXL_20221203_181515838_mid.jpg", timestamp: 1670091315838, tags: ["outside", "landscape"], notes: "" },
   ],
   tags: ["document", "outside", "cat", "cat-loaf", "my-room", "body-in-view", "food", "sunset-sunrise", "landscape", "beautiful-sky"]
 };
